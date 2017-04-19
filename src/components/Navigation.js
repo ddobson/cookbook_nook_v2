9f60ac02ac5d4c7649fc70f822a033f8d12ccbf3
ddobson <ddobson445@gmail.com>
2017-04-19 17:58:16 -0400

    Implement navigation
    - Install react-burger-menu
    - implement burger menu
    - style navigation
    - add logo svg

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { slide as Menu } from 'react-burger-menu';
+import { NavLink } from 'react-router-dom';
+import logo from '../images/logo.svg';
+
+import '../styles/Navigation.scss';
 
 class Navigation extends React.Component {
   render() {
     return (
-      <div className="Navigation">
-        Nav
+      <div className="nav-bar">
+        <img src={logo} className="logo" alt="cookbook-nook-logo" />
+        <Menu right>
+          <div className="nav-item">
+            <NavLink to="sign-up">Sign Up</NavLink>
+          </div>
+          <div className="nav-item">
+            <NavLink to="sign-in">Sign In</NavLink>
+          </div>
+        </Menu>
       </div>
     );
   }
