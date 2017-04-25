17dac1ac8771ea104aa73bc2fbecc2559b221a68
ddobson <ddobson445@gmail.com>
2017-04-25 16:18:21 -0400

    Create an Empy CookbookContainer
    - Empty component for showing a cookbook's recipes

---
+++ src/components/CookbookContainer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class CookbookContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cookbook: {}
+    };
+  }
+
+  componentDidMount() {
+    
+  }
+
+  render() {
+
+  }
+}
+
+export default CookbookContainer;
